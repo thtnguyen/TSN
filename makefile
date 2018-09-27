36b1b4cddbcdd87700a2d0a7ec2f5bddff5eff03
Emertxe <alextho4@gmail.com>
2018-09-27 04:06:51 -0500

    main.cpp: Added a simple menu, still has a few bugs to be worked out. 	Still not 100% sure how Opensplice DDS works, so if there's any obvious issues, feel free to point it out
    main.cpp.bak: Original code pre-menu addition
    
    makefile: Simply changed stalker to exe in the clean macro
    
    Signed-off-by: Emertxe <alextho4@gmail.com>

--- makefile
+++ makefile
@@ -46,6 +46,6 @@ exe: ${IDL_GENERATED_H} ${IDL_GENERATED_CPP} ${COMMON_H} ${COMMON_CPP} src/main.
 #	g++ -o $@ ${CFLAGS} ${CXXFLAGS} $^ ${LIBS}
 
 clean:
-	-rm -f stalker
+	-rm -f exe
 	-rm -f ${IDL_GENERATED_H} ${IDL_GENERATED_CPP}
 	-rm -f ospl-error.log ospl-info.log
--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,9 @@
 
 using namespace TSN;
 
+char name[100];
+int state;
+
 void background(char* name)
 {
 	MsgSeq msgList;
@@ -21,8 +24,8 @@ void background(char* name)
 
 	mgr.createParticipant("TSN");
 
-    MsgTypeSupport_var mt = new MsgTypeSupport();
-    mgr.registerType(mt.in());
+  MsgTypeSupport_var mt = new MsgTypeSupport();
+  mgr.registerType(mt.in());
 
 	char topic[] = "Topic";
 	mgr.createTopic(topic);
@@ -34,54 +37,58 @@ void background(char* name)
 	MsgDataReader_var MsgReader = MsgDataReader::_narrow(dr.in());
 	checkHandle(MsgReader.in(), "MsgDataReader::_narrow");
 
-  std::cout << "=== [Subscriber] Ready ..." << std::endl;
+  //std::cout << "=== [Subscriber] Ready ..." << std::endl;
 
   //bool closed = false;
   ReturnCode_t status =  - 1;
   //int count = 0;
-	
+
 	while(1)
 	{
 		status = MsgReader->take(msgList, infoSeq, LENGTH_UNLIMITED,
       ANY_SAMPLE_STATE, ANY_VIEW_STATE, ANY_INSTANCE_STATE);
     checkStatus(status, "msgDataReader::take");
-    for (DDS::ULong j = 0; j < msgList.length(); j++)
-    {
-			//std::cout << "my name is " << name << " and sender name is " << msgList[0].name << std::endl;
-			if(strcmp(msgList[j].name, name) != 0)
-			{
-      std::cout << "=== [Subscriber] message received :" << std::endl;
-      std::cout << "    Name  : " << msgList[j].name << std::endl;
-      std::cout << "    Message : \"" << msgList[j].message << "\"" << std::endl;
-      //closed = true;
-			}
-    }
-    status = MsgReader->return_loan(msgList, infoSeq);
-    checkStatus(status, "MsgDataReader::return_loan");
+
+		//run when in read message state
+		if(state == 1)
+		{
+		  for (DDS::ULong j = 0; j < msgList.length(); j++)
+		  {
+				//std::cout << "my name is " << name << " and sender name is " << msgList[0].name << std::endl;
+				if(strcmp(msgList[j].name, name) != 0)
+				{
+		    std::cout << "=== [Subscriber] message received :" << std::endl;
+		    std::cout << "    Name  : " << msgList[j].name << std::endl;
+		    std::cout << "    Message : \"" << msgList[j].message << "\"" << std::endl;
+		    //closed = true;
+				}
+		  }
+			
+		}
+
+		status = MsgReader->return_loan(msgList, infoSeq);
+		checkStatus(status, "MsgDataReader::return_loan");    
 		sleep(1);
-	}
+	}	
 
   mgr.deleteReader();
   mgr.deleteWriter();
   mgr.deletePublisher();
   mgr.deleteTopic();
   mgr.deleteParticipant();
+
 }
 
-int main (int argc, char* argv[])
+void printMenu()
 {
-  std::cout << "Welcome to The Social Network." << std::endl;
+	std::cout << "Select what you want to do:" << std::endl << std::endl;
+	std::cout << "(1) Recieve Messages" << std::endl;
+	std::cout << "(2) Publish Messages" << std::endl;
+	std::cout << "Type any other key to Exit" << std::endl;
+}
 
-	std::cout << "Enter name: " << std::endl;
-	char name[100];
-	std::cin >> name;
-	std::cin.ignore();
-
-	std::thread BG (background, name);
-	//user this_user = load_user_data(".tsn");
-
-	//code to publish startup info goes here
-
+void publishMessages(bool kill)
+{
 	DDSEntityManager mgr;
 	mgr.createParticipant("TSN");
 
@@ -92,51 +99,91 @@ int main (int argc, char* argv[])
 	mgr.createTopic(topic);
 
 	mgr.createPublisher();
-
 	mgr.createWriter(false);
 
 	DataWriter_var dw = mgr.getWriter();
-
 	MsgDataWriter_var MsgWriter = MsgDataWriter::_narrow(dw.in());
-	int choice;
 
-	while(1)
+	std::cout << "Enter msg: " << std::endl;
+	char msg[255];	
+	std::cin >> msg;
+	std::cin.ignore();
+
+	Msg msgInstance;
+	msgInstance.name = DDS::string_dup(name);
+	msgInstance.message = DDS::string_dup(msg);
+
+	std::cout << "=== [Publisher] writing a message containing :" << std::endl;
+	std::cout << "    Name  : " << msgInstance.name << std::endl;
+	std::cout << "    Message : \"" << msgInstance.message << "\"" << std::endl;
+
+	ReturnCode_t status = MsgWriter->write(msgInstance, DDS::HANDLE_NIL);
+	checkStatus(status, "MsgDataWriter::write");
+
+	state = -1;
+	printMenu();
+}
+
+void viewMessages()
+{
+	std::cout << "=== RECIEVE MESSAGE MODE" << endl;
+	std::cout << "Type 0 to exit" << endl;
+
+	while(state == 1)
 	{
+		std::cin >> state;
 		std::cin.ignore();
-		std::cin >> choice;
-		if(choice == 1)
-		{
-			std::cout << "Enter msg: " << std::endl;
-			char msg[255];
-			std::cin.ignore();
-			std::cin >> msg;
-
-			Msg msgInstance;
-			msgInstance.name = DDS::string_dup(name);
-			msgInstance.message = DDS::string_dup(msg);
-
-			std::cout << "=== [Publisher] writing a message containing :" << std::endl;
-			std::cout << "    Name  : " << msgInstance.name << std::endl;
-			std::cout << "    Message : \"" << msgInstance.message << "\"" << std::endl;
-
-			ReturnCode_t status = MsgWriter->write(msgInstance, DDS::HANDLE_NIL);
-			checkStatus(status, "MsgDataWriter::write");
-			sleep(1);
-		}
-		else
-		{
-			exit(0);
-		}
 	}
-	
-  BG.join();
-	
-  mgr.deleteReader();
-  mgr.deleteWriter();
-  mgr.deletePublisher();
-  mgr.deleteTopic();
-  mgr.deleteParticipant();
 
+	state = -1;	
+	printMenu();
+}
+
+void menu()
+{
+	printMenu();
+	state = -1;
+
+	while(state != 0)
+	{
+		std::cin >> state;
+		std::cin.ignore();
+		if(state == 0)
+		{
+			//exit loop and return to main method
+			break;
+		}		
+		if(state == 1)
+		{
+			viewMessages();
+		}
+		if(state == 2)
+		{
+			publishMessages(false);
+		}
+		state = -1;
+	}
+
+	publishMessages(true);
+	std::cout << "EXITING: STATE = " << state << std::endl;
+}
+
+int main (int argc, char* argv[])
+{	
+	std::cout << "Welcome to The Social Network." << std::endl;
+
+	std::cout << "Enter name: " << std::endl;
+	std::cin >> name;
+	std::cin.ignore();
+
+	std::thread BG(background, name);
+
+	//enter menu state
+	menu();
+
+	exit(0);
+
+	BG.join();
   return 0;
 }
 
